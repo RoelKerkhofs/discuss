65af57831b8e244ca48dbe932cfe298aa91b8f8d
Roel Kerkhofs <roel@Roels-MacBook-Air.local>
2018-12-26 11:55:41 +0100

    Cleaned up dependencies and added ecto_sql to applications

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Discuss.Mixfile do
   def application do
     [mod: {Discuss, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :ecto_sql]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,7 +38,6 @@ defmodule Discuss.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:ecto_sql, "~> 3.0.3"},
-     #{:telemetry, ">= 0.0.0"},
      {:cowboy, "~> 1.0"}]
   end
 
