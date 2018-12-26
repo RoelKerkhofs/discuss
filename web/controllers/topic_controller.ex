ff6d26027b5e108abd99d8e1a094bc78b485268a
Roel Kerkhofs <roel@Roels-MacBook-Air.local>
2018-12-26 14:11:54 +0100

    Lecture 86 - setting up edit route and controller function

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,6 +3,11 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
@@ -22,9 +27,6 @@ defmodule Discuss.TopicController do
     end
   end
 
-  def index(conn, _params) do
-    topics = Repo.all(Topic)
-    render conn, "index.html", topics: topics
-  end
+  def edit(conn, %{"id" => topic_id})
 
 end
