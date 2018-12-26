f446db308e656d8eb046568bf8b1d651c254685c
Roel Kerkhofs <roel@Roels-MacBook-Air.local>
2018-12-26 22:39:18 +0100

    Lecture 95 - Add dependencies for ueberauth

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Discuss.Mixfile do
   def application do
     [mod: {Discuss, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :ecto_sql]]
+                    :phoenix_ecto, :postgrex, :ecto_sql, :ueberauth, :ueberauth_github]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,7 +38,9 @@ defmodule Discuss.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:ecto_sql, "~> 3.0.3"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:ueberauth, "~> 0.3"},
+     {:ueberauth_github, "~> 0.4"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
