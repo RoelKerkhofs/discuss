72c6e61270adddb36ff0b60bd0eab704121acd6d
Roel Kerkhofs <roel@Roels-MacBook-Air.local>
2018-12-29 20:56:30 +0100

    Included authentication

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -4,6 +4,7 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic
 
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:edit, :update, :delete]
 
   def index(conn, _params) do
     IO.inspect(conn.assigns)
@@ -18,7 +19,10 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+
+    changeset = conn.assigns.user
+    |> build_assoc(:topics)
+    |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _topic} ->
@@ -57,5 +61,18 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "I'm sorry Dan, I cant let you do that")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 end
--- web/models/topic.ex
+++ web/models/topic.ex
@@ -3,6 +3,7 @@ defmodule Discuss.Topic do
 
   schema "topics" do
       field :title, :string
+      belongs_to :user, Discussion.User
   end
 
   def changeset(struct, params \\ %{}) do
