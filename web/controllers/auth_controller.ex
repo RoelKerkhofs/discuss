0800fd7bdd3f6ad7da005b29c2fb907f39ae2c1e
Roel Kerkhofs <roel@Roels-MacBook-Air.local>
2018-12-29 12:13:42 +0100

    working completely with auth editing and deleting

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -34,4 +34,11 @@ defmodule Discuss.AuthController do
         {:ok, user}
     end
   end
+
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: topic_path(conn, :index))
+  end
+
 end
